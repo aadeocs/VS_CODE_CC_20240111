f061ef4a75dac9a0fca681042d86f2d84a959e71
aadeocs <dave@deocs.com>
2024-01-13 05:27:27 -0500

    Manual set partial pivot vector ok

--- GaussJord_vA/GJ.cc
+++ GaussJord_vA/GJ.cc
@@ -9,6 +9,7 @@ float Mata[ROWS][COLS];         // Matrix a, working version of A
 float ColB[ROWS];               // Column vector B, original
 float Colb[ROWS];               // Column vector b, working version of B
 float ColX[ROWS];               // Column vector X
+int  Pvt[ROWS];
 
 /*--------------------------------------------------------------
 | Prepare matrices and column vectors
@@ -17,11 +18,11 @@ void FillAXB()
 {
     Mata[0][0]=2; Mata[0][1]=1; Mata[0][2]=1;
     Mata[1][0]=1; Mata[1][1]=2; Mata[1][2]=1;
-    Mata[2][0]=3.01; Mata[2][1]=3.01; Mata[2][2]=2.01;
-    //Mata[2][0]=1; Mata[2][1]=1; Mata[2][2]=2;
-    Colb[0]=ColB[0]=4;
-    Colb[1]=ColB[1]=4;
-    Colb[2]=ColB[2]=8;
+    //Mata[2][0]=3.01; Mata[2][1]=3.01; Mata[2][2]=2.01;
+    Mata[2][0]=1; Mata[2][1]=1; Mata[2][2]=2;
+    Colb[0]=ColB[0]=1;  //4;
+    Colb[1]=ColB[1]=2;  //4;
+    Colb[2]=ColB[2]=3;  //8;
     for(int r=0; r<ROWS; r++)
     {
         MatA[r][0]=Mata[r][0]; MatA[r][1]=Mata[r][1]; MatA[r][2]=Mata[r][2];
@@ -103,26 +104,29 @@ void Printab(char *msg, int row, bool print_x=false)
 --------------------------------------------------------------*/
 bool GaussianElimination(float mat_a[ROWS][COLS], float col_x[COLS],float col_b[COLS])
 {
+    Pvt[0]=1;
+    Pvt[1]=2;
+    Pvt[2]=0;
     // elimination
     for(int c = 0; c<COLS; c++)                     // For all cols
     {
-        float piv = mat_a[c][c];
+        float piv = mat_a[ Pvt[c]][c];
         for(int cp=0; cp<COLS; cp++)                // Normalize the cth row
-            mat_a[c][cp] /= piv;
-        col_b[c]/=piv;
+            mat_a[ Pvt[c]][cp] /= piv;
+        col_b[ Pvt[c]]/=piv;
         Printab("Apply Pivot",c);
         for(int r=c;r<ROWS;r++)                     // For all rows in that col
         {
             if(r!=c)                                // only process non-cth row
             {
-                float dv = mat_a[r][c];
+                float dv = mat_a[ Pvt[r]][c];
                 for(int cp=0; cp<COLS; cp++)        // for all cols (could start at c)
-                    mat_a[r][cp] /= dv;              // normalize row
-                col_b[r]/=dv;                        // normalize b
+                    mat_a[ Pvt[r]][cp] /= dv;              // normalize row
+                col_b[ Pvt[r]]/=dv;                        // normalize b
                 Printab("Normalize Row",r);
                 for(int cp=0; cp<COLS; cp++)        // for all cols (could start at c)
-                    mat_a[r][cp] -= mat_a[c][cp];     // subtract ref row from current row
-                col_b[r]-=col_b[c];                   // subtract ref b from current b
+                    mat_a[ Pvt[r]][cp] -= mat_a[ Pvt[c]][cp];     // subtract ref row from current row
+                col_b[ Pvt[r]]-=col_b[ Pvt[c]];                   // subtract ref b from current b
                 Printab("Zero Below Pivot",r);
             }
         }
@@ -130,10 +134,10 @@ bool GaussianElimination(float mat_a[ROWS][COLS], float col_x[COLS],float col_b[
     // back substitute
     for(int r=ROWS-1;r>=0;r--)          // r: 2, 1, 0, then c: 2, 2 1, 2 1 0
     {
-        col_x[r]=col_b[r];                    // start with b
+        col_x[ Pvt[r]]=col_b[ Pvt[r]];                    // start with b
         for(int c=r+1; c<COLS; c++)         // c: 2, 2 1, 2 1 0
         {
-            col_x[r]-=mat_a[r][c]*col_x[c];    // subtract precompute terms
+            col_x[ Pvt[r]]-=mat_a[ Pvt[r]][c]*col_x[ Pvt[c]];    // subtract precompute terms
         }
     }
     return true;
